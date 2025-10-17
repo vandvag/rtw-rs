5da7379d11115d7dc22c4e716ce9a6385a5cfd8c
Evangelos Vandoros <vandvag@gmail.com>
2025-10-17 18:27:06 +0300

    Refactored main loop to use functional style

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,41 @@
 use glam::DVec3;
-use indicatif::{ProgressBar, ProgressStyle};
+use indicatif::{ProgressIterator, ProgressStyle};
+use itertools::Itertools;
 
 fn main() {
     let image_width = 256;
     let image_height = 256;
 
-    let pb = ProgressBar::new(image_height);
-    pb.set_style(
-        ProgressStyle::default_bar()
-            .template("{msg} [{bar:40.cyan/blue}] {pos:>3}/{len:3}")
-            .unwrap()
-            .progress_chars("=>-"),
-    );
-
-    pb.set_message("Raytracing!");
+    let progress_style = ProgressStyle::default_bar()
+        .template("Raytracing! [{bar:40.cyan/blue}] {pos:>3}/{len:3}")
+        .unwrap()
+        .progress_chars("=>-");
 
     println!("P3\n{} {}\n255\n", image_width, image_height);
 
     let image_width_d = image_width as f64;
     let image_height_d = image_height as f64;
-    for height in (0..image_height).map(|x| x as f64) {
-        for width in (0..image_width).map(|x| x as f64) {
-            let r = width / (image_width_d - 1.0);
-            let g = height / (image_height_d - 1.0);
+
+    let pixels = (0..image_height)
+        .cartesian_product(0..image_width)
+        .collect::<Vec<(u64, u64)>>()
+        .into_iter()
+        .progress_count(image_height * image_width)
+        .with_style(progress_style)
+        .map(|(h, w)| {
+            let r = w as f64 / (image_width_d - 1.0);
+            let g = h as f64 / (image_height_d - 1.0);
             let pixel_color = DVec3::new(r, g, 0.0);
 
-            println!(
+            format!(
                 "{} {} {}",
                 (255.999 * pixel_color.x) as u8,
                 (255.999 * pixel_color.y) as u8,
                 (255.999 * pixel_color.z) as u8
-            );
-        }
+            )
+        })
+        .collect::<Vec<String>>()
+        .join("\n");
 
-        pb.inc(1);
-    }
+    println!("{pixels}")
 }
