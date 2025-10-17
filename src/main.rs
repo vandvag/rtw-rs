74aef669057835a8732a904bfac11bfcd8df334c
Evangelos Vandoros <vandvag@gmail.com>
2025-10-17 18:49:54 +0300

    Added camera math

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 use glam::DVec3;
 use indicatif::{ProgressIterator, ProgressStyle};
 use itertools::Itertools;
+use rtw::ray::Ray;
 
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = if (image_width as f64) / aspect_ratio < 1.0 {
+        1
+    } else {
+        ((image_width as f64) / aspect_ratio) as u64
+    };
+    let focal_length = 1.0;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
+    let camera_center = DVec3::ZERO;
+    let viewport_u = DVec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = DVec3::new(0.0, -viewport_height, 0.0);
+    let pixel_delta_u = viewport_u / image_width as f64;
+    let pixel_delta_v = viewport_v / image_height as f64;
+    let viewport_upper_left =
+        camera_center - DVec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel00_location = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     let progress_style = ProgressStyle::default_bar()
         .template("Raytracing! [{bar:40.cyan/blue}] {pos:>3}/{len:3}")
@@ -13,9 +30,6 @@ fn main() {
 
     println!("P3\n{} {}\n255\n", image_width, image_height);
 
-    let image_width_d = image_width as f64;
-    let image_height_d = image_height as f64;
-
     let pixels = (0..image_height)
         .cartesian_product(0..image_width)
         .collect::<Vec<(u64, u64)>>()
@@ -23,9 +37,10 @@ fn main() {
         .progress_count(image_height * image_width)
         .with_style(progress_style)
         .map(|(h, w)| {
-            let r = w as f64 / (image_width_d - 1.0);
-            let g = h as f64 / (image_height_d - 1.0);
-            let pixel_color = DVec3::new(r, g, 0.0);
+            let pixel_center =
+                pixel00_location + (w as f64 * pixel_delta_u) + (h as f64 * pixel_delta_v);
+            let ray = Ray::new(camera_center, pixel_center - camera_center);
+            let pixel_color = ray.color();
 
             format!(
                 "{} {} {}",
--- src/ray.rs
+++ src/ray.rs
@@ -9,4 +9,10 @@ impl Ray {
     pub fn new(origin: DVec3, direction: DVec3) -> Self {
         Self { origin, direction }
     }
+
+    pub fn color(&self) -> DVec3 {
+        let unit_dir = self.direction.normalize();
+        let a = 0.5 * unit_dir.y + 1.0;
+        DVec3::lerp(DVec3::ONE, DVec3::new(0.5, 0.7, 1.0), a)
+    }
 }
